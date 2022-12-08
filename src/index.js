15cc8098f9a0781c703ded9051a82206ed56f48e
hungpham128 <hungdbi1281999@gmail.com>
2022-12-08 23:04:37 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const { engine } = require('express-handlebars');
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('.hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
